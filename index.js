2c779a7252b4209bd2987259b7888a5f63a497da
PiechBartosz <p.bartosz@yahoo.com>
2024-07-04 12:26:40 +0200

    changes

--- index.js
+++ index.js
@@ -20,3 +20,56 @@ const activeNavigation = () => {
 burgerBtn.addEventListener("click", activeNavigation);
 
 // end mobile navigation
+
+// start accordion
+
+const accordionBtns = document.querySelectorAll(".accordion__btn");
+
+function openAccordionTextBox() {
+	const textBox = this.nextElementSibling;
+	const iconSecond = this.querySelector(".accordion__icon__second");
+
+	if (textBox.classList.contains("accordion__active")) {
+		textBox.classList.remove("accordion__active");
+		iconSecond.classList.remove("rotate");
+	} else {
+		closeAccordionTextBox();
+		textBox.classList.add("accordion__active");
+		iconSecond.classList.add("rotate");
+	}
+}
+
+function closeAccordionTextBox() {
+	const allActiveTextboxes = document.querySelectorAll(
+		".accordion__text-box.accordion__active"
+	);
+	const allActiveIcons = document.querySelectorAll(
+		".accordion__icon__second.rotate"
+	);
+
+	allActiveTextboxes.forEach((textBox) => {
+		textBox.classList.remove("accordion__active");
+	});
+
+	allActiveIcons.forEach((icon) => {
+		icon.classList.remove("rotate");
+	});
+}
+
+accordionBtns.forEach((btn) =>
+	btn.addEventListener("click", openAccordionTextBox)
+);
+
+// end accordion
+
+// start footer
+const footerYear = document.querySelector(".footer__year");
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+
+// end footer
